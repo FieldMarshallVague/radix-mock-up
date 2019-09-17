4c89fb50a2c5c23fe2d1466505fd5d3a5c075915
Toby Worth <tobyworth@hotmail.com>
2019-09-17 22:59:11 +0100

    moved navbar to component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Navbar />
     <router-view />
   </div>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+import Navbar from "@/components/Navbar.vue"; // @ is an alias to /src
+
+export default Vue.extend({
+  name: "home",
+  components: {
+    Navbar
+  }
+});
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link>|
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Navbar",
+  props: {
+  }
+});
+</script>
+
+<style scoped lang="scss">
+
+</style>
