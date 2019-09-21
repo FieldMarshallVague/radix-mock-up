f7f1fef637e1d320e250fbe066ecb47628111cf6
Toby Worth <tobyworth@hotmail.com>
2019-09-21 02:31:19 +0100

    added footer

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <div id="app">
     <Navbar />
-    <router-view />
+    <div class="router-view-container">
+      <router-view />
+    </div>
+    <Footer />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import Navbar from "@/components/Navbar.vue"; // @ is an alias to /src
+import Footer from "@/components/Footer.vue"; 
 
 export default Vue.extend({
   name: "home",
   components: {
-    Navbar
+    Navbar,
+    Footer
   }
 });
 </script>
@@ -22,5 +27,21 @@ export default Vue.extend({
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  min-height: 100%;
 }
+
+.router-view-container {  
+  -webkit-box-flex: 1;
+  -ms-flex: 1;
+  flex: 1;
+}
+
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,117 @@
+<template>
+  <footer>
+    <div class="footer-content">
+      <div class="">
+          <h2>LOGO</h2>
+      </div>
+      <div class="links1">
+        <ul>
+          <li class="nav-item"><router-link class="nav-link" to="/">Home</router-link></li>
+          <li class="nav-item"><router-link class="nav-link" to="/discovery">Discovery</router-link></li>
+          <li class="nav-item"><router-link class="nav-link" to="/photos">Photos</router-link></li>
+          <li class="nav-item"><router-link class="nav-link" to="/contact">Contact</router-link></li>
+        </ul>
+      </div>
+      <div class="links2">
+        <ul>
+          <li class="nav-item"><router-link class="nav-link" to="/">About</router-link></li>
+          <li class="nav-item"><router-link class="nav-link" to="/discovery">Help</router-link></li>
+          <li class="nav-item"><router-link class="nav-link" to="/photos">Terms</router-link></li>
+          <li class="nav-item"><router-link class="nav-link" to="/contact">Guidelines</router-link></li>
+        </ul>
+      </div>
+      <div class="links3">
+        <ul>
+          <li class="nav-item"><router-link class="nav-link" to="/">Testimonials</router-link></li>
+          <li class="nav-item"><router-link class="nav-link" to="/discovery">Advertise</router-link></li>
+          <li class="nav-item"><router-link class="nav-link" to="/photos">Integrations</router-link></li>
+          <li class="nav-item"><router-link class="nav-link" to="/contact">Careers</router-link></li>
+        </ul>
+      </div>
+      <div class="search">
+        <input type="text" placeholder="Email" />
+        <span>Stay in touch with us for the freshest products!</span>
+      </div>
+    </div>    
+  </footer>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+
+});
+</script>
+
+<style scoped lang="scss">
+footer{
+  width:100%;
+  background-color: #A6ADB4;
+  color: #fff;
+  clear: both;
+  position: relative;
+  height: 200px;
+  margin-top: -200px;
+}
+
+.footer-content {
+  width: 800px;
+  margin-left:auto;
+  margin-right:auto;
+
+  display:grid;  
+  align-content: start;
+  grid-template-rows: auto;
+  grid-template-columns: repeat(12, 1fr [col-start]);
+  grid-gap:var(--grid-gap);
+  grid-template-areas:
+    "logo links1 links2 links3 . . search search search . social social";
+    
+  @media screen and (max-width: 992px){
+    // grid-template-columns: auto;
+    // grid-template-rows: 80px 100px 80px;
+    // grid-template-areas: 
+    //   "hero . ."
+    //   "."
+    //   ".";
+  }
+}
+
+ul li {
+  list-style-type: none;
+
+}
+
+.logo {
+  border: 1px solid red;
+  grid-area: logo;
+}
+
+.links1 {
+  grid-area: links1;
+}
+
+.links2 {
+  grid-area: links2;
+}
+
+.links3 {
+  grid-area: links3;
+}
+
+.search {
+  grid-area: search;
+
+  input[type=text]{
+    width:100%;
+    border:2px solid var(--footer-border-color)
+  }
+}
+
+.social {
+  grid-area: social;
+}
+</style>
+
+
